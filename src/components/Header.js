55a9fce4b1232f27a771f735a46f620a8105ba80
Md Alamin <mdalamin88248@gmail.com>
2022-12-24 21:25:02 +0600

    add navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,27 @@
-import React from 'react';
-
-const Header = () => {
-  return <div>Header</div>;
+import React from "react";
+import Logo from "../assets/img/logo.svg";
+import Nav from "./Nav";
+import { HiMenu } from "react-icons/hi";
+const Header = ({ setNavMobile }) => {
+  return (
+    <header className="py-6">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          {/* logo */}
+          <a href="#">
+            <img src={Logo} className="h-[30px]" alt="" />
+          </a>
+          {/* nav */}
+          <Nav />
+          {/* nav mobile */}
+          <HiMenu
+            onClick={() => setNavMobile(true)}
+            className="lg:hidden text-3xl text-white cursor-pointer"
+          />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,7 +1,28 @@
-import React from 'react';
-
-const NavMobile = () => {
-  return <div>NavMobile</div>;
+import React from "react";
+import { IoClose } from "react-icons/io5";
+const NavMobile = ({ setNavMobile }) => {
+  return (
+    <nav className="lg:hidden bg-[#251f3f] w-full h-full">
+      <IoClose
+        onClick={() => setNavMobile(false)}
+        className="text-3xl absolute left-4 top-6 cursor-pointer"
+      />
+      <ul className="flex flex-col items-center justify-center h-full space-y-8 font-secondary">
+        <li className="nav-link">
+          <a href="#">Home</a>
+        </li>
+        <li className="nav-link">
+          <a href="#">Company</a>
+        </li>
+        <li className="nav-link">
+          <a href="#">Features</a>
+        </li>
+        <li className="btn">
+          <a href="#">Signup</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavMobile;
